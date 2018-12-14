a9f00c186d1b77e0cc05f3d1d09b45f8cfdb8ee5
Felix Stephen <felsen88@gmail.com>
2018-12-14 14:51:54 +0545

    Modified: Vector2d

---
+++ vector2d.py
@@ -0,0 +1,35 @@
+
+from array import array
+import math
+
+
+class Vector2d(object):
+
+    typecode = 'd'
+    
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        return "{} ({} {})".format(self.__name__, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(array(self.typecode, self)))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    
