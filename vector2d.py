a3c67a624e3a3023d452cdb151c7cc32175fd6bc
Felix Stephen <felsen88@gmail.com>
2018-12-14 15:15:36 +0545

    Modified: __iter__, __bytes__, __eq__, __abs__, __bool__

--- vector2d.py
+++ vector2d.py
@@ -33,3 +33,21 @@ class Vector2d(object):
         return bool(abs(self))
 
     
+class Vector1d(object):
+
+    typecode = 'd'
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
