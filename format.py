823ad210fb94fc71344484031589769621ea764c
Felix Stephen <felsen88@gmail.com>
2018-12-17 15:33:24 +0545

    Modified: __format__

--- format.py
+++ format.py
@@ -3,6 +3,9 @@ custom string formating, and how it works.
 """
 
 
+from datetime import datetime
+
+
 class Date(object):
 
     _formats = {
@@ -16,14 +19,29 @@ class Date(object):
         self.month = month
         self.year = year
 
+    # def __format__(self, code):
+    #     """
+    #     Supports custom date formats as defined.
+    #     """
+    #     if code == "":
+    #         code = "ymd"
+    #     fmt = self._formats[code]
+    #     return fmt.format(d=self)
+
     def __format__(self, code):
+        """
+        Supports generic date formats.
+        """
+        dfmt = "-".join((str(self.day), str(self.month), str(self.year)))
+        d = datetime.strptime(dfmt, "%Y-%m-%d")
         if code == "":
-            code = "ymd"
-        fmt = self._formats[code]
-        return fmt.format(d=self)
+            code = "%A, %B %d, %Y"
+        return format(d, code)
 
 
-d = Date(2018, 12, 12)
+d = Date(2018, 2, 12)
+# print(format(d))
+# print(format(d, "dmy"))
+# print(format(d, "mdy"))
 print(format(d))
-print(format(d, "dmy"))
-print(format(d, "mdy"))
+print(format(d, "%Y-%m-%d"))
