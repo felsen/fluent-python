7ed5acd5b7f3419bf970d3675dd6e768f64b68a8
Felix Stephen <felsen88@gmail.com>
2018-12-18 15:01:45 +0545

    Modified: property

--- property.py
+++ property.py
@@ -32,3 +32,34 @@ name.first_name = "Stephen"
 print(name.first_name)
 # name.first_name = 23 # Exception as raised
 # del name.first_name  # Exception as raised
+
+
+class PersonOne(object):
+
+    def __init__(self, first_name):
+        self.set_first_name(first_name)
+
+    # Getter function
+    def get_first_name(self):
+        return self._first_name
+
+    # Setter function
+    def set_first_name(self, value):
+        if not isinstance(value, str):
+            raise AttributeError("Expected as string!")
+        self._first_name = value
+
+    # Deleter function
+    def del_first_name(self):
+        raise AttributeError("cannot be deleted!")
+
+    name = property(get_first_name, set_first_name, del_first_name)
+
+
+po = PersonOne('Felix Stephen')
+print(po.get_first_name)
+po.set_first_name = 'stephen'
+print(po.get_first_name)
+print(po.del_first_name)
+
+    
