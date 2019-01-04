0d42e895b3eae12187f945bc3b6a8e63b5b42f32
Felix Stephen <felsen88@gmail.com>
2019-01-04 12:42:21 +0545

    Modified: original magic method calling using super

--- parents.py
+++ parents.py
@@ -42,9 +42,43 @@ class D(C):
         self.y = 2
         print(self.y)
 
+print("----------")
 c = C()
 c # 1
 d = D()
 d # 1
   # 2
 
+
+class E(D, C):
+
+    def __init__(self):
+        super().__init__()
+        self.z = 3
+        print(self.z)
+
+print("------------")
+e = E()
+e # 1
+  # 2
+  # 3
+
+
+class Proxy(object):
+
+    def __init__(self, obj):
+        self._obj = obj
+
+    def __getattr__(self, name):
+        return getattr(self._obj, name)
+
+    def __setattr__(self, name, value):
+        # another usage of super is override the any magic methods.
+        # this will call the original __setattr__
+        if name.startswith('_'):
+            super().__setattr__(name, value)
+        else:
+            setattr(self._obj, name, value)
+
+
+  
